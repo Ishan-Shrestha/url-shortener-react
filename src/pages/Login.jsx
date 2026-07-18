1879b8f92004e483cc632f9d053a14f42442220a
Ishan-Shrestha <sthaishan218@gmail.com>
2026-07-18 11:38:31 +0545

    login page with JWT auth

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,50 @@
-function Login(){
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
 
+function Login(){
+    const [ id, setId ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const navigate = useNavigate();
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        const payload = {
+            'id': id,
+            'password':password
+        }
+        try{
+            const response = await fetch('http://127.0.0.1:5000/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(payload)
+                })
+            if (!response.ok){
+                throw new Error(`Error: ${response.status}`);
+            }
+            const data = await response.json();
+            if (data.success){
+                localStorage.setItem('token', data.token);
+                navigate('/shorten');
+            } else {
+                alert(data.message);
+            }
+        }
+        catch(error){
+            console.log(`Fetch Error: ${error}`);
+        }
+    }
     return(
         <>
-        <h1>Login</h1>
+        <form onSubmit={ handleSubmit }>
+            <input type="text" 
+                value={id} 
+                onChange={(e)=>setId(e.target.value)} 
+                placeholder="Enter Your ID"></input>
+            <input type="password" 
+                value={password} 
+                onChange={(e)=>setPassword(e.target.value)} 
+                placeholder="Enter Your password"></input>
+            <button type="submit">submit</button>
+        </form>
         </>
     )
 }
